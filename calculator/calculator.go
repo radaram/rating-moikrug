3f91c5f661e044f3983c804fc3ddb7a0a56dba80
Rail Alimov <ra.alimov@gmail.com>
2019-07-26 20:48:18 +0700

    fix typo

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,57 +1,52 @@
 package main
 
 import (
-	"log"
 	"database/sql"
+	"log"
 )
-
 
 func calculate(company Company) {
 	log.Println(company)
-	total_score := company.calculateTotalScore() 
+	total_score := company.calculateTotalScore()
 	log.Println("total_score", total_score)
 	company.Score = total_score
 	saveResult(company)
 }
 
-
 func saveResult(company Company) {
 	var company_id int
 	employeesLeftData, err := company.employeesLeftJsonEncode()
 	failOnError(err, "json encoded error")
-    
+
 	employeesCameData, err := company.employeesCameJsonEncode()
 	failOnError(err, "json encoded error")
 
 	err = db.QueryRow("SELECT id FROM company WHERE name = $1", company.Name).Scan(&company_id)
 	if err == sql.ErrNoRows {
 		_, err = db.Exec(
-			"INSERT INTO company (name, site, about, raiting, address, score, link, employees_left, employees_came) " +
-			"VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", 
-			company.Name, company.Site, 
+			"INSERT INTO company (name, site, about, rating, address, score, link, employees_left, employees_came) "+
+				"VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+			company.Name, company.Site,
 			company.About, company.Rating,
-			company.Address, company.Score, 
+			company.Address, company.Score,
 			company.Link,
 			string(employeesLeftData), string(employeesCameData),
 		)
 		failOnError(err, "insert error")
 		log.Println("INSERT", company.Name)
 	} else if err != nil {
-        failOnError(err, "get company error")
-    } else {
-		_, err = db.Exec("UPDATE company " + 
-				"SET site = $1, about = $2, rating = $3, address = $4, " +
-				"    score = $5, link = $6, employee_left = $7, employee_came = $8 " +
-			    "WHERE id = $8", 
-				 company.Site, company.About, 
-				 company.Rating, company.Address, company.Score, 
-				 company.Link,
-				 string(employeesLeftData), string(employeesCameData),
-				 company_id)
+		failOnError(err, "get company error")
+	} else {
+		_, err = db.Exec("UPDATE company "+
+			"SET site = $1, about = $2, rating = $3, address = $4, "+
+			"    score = $5, link = $6, employee_left = $7, employee_came = $8 "+
+			"WHERE id = $8",
+			company.Site, company.About,
+			company.Rating, company.Address, company.Score,
+			company.Link,
+			string(employeesLeftData), string(employeesCameData),
+			company_id)
 		failOnError(err, "update error")
 		log.Println("UPDATE", company.Name)
-	} 
+	}
 }
-
-
-
--- parser/parser.go
+++ parser/parser.go
@@ -2,79 +2,79 @@ package main
 
 import (
 	"log"
-	"sync"
-	"strings"
 	"strconv"
+	"strings"
+	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func grabCompanies(url string, c chan *Company, wg *sync.WaitGroup) {
-	
+
 	log.Println(url)
 	doc, err := goquery.NewDocument(url)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+
 	doc.Find(".companies-item").Each(func(i int, s *goquery.Selection) {
 		company_page, _ := s.Find(".title").Attr("href")
 		company_page = BASE_URL + company_page
-	    wg.Add(1)
+		wg.Add(1)
 		go grabCompany(company_page, c, wg)
-    })
+	})
 
 	next_page, _ := doc.Find(".pagination .next_page").Attr("href")
-	if (next_page != "") {
-		grabCompanies(BASE_URL + next_page, c, wg)
+	if next_page != "" {
+		grabCompanies(BASE_URL+next_page, c, wg)
 	}
 }
 
-
 func grabCompany(url string, c chan *Company, wg *sync.WaitGroup) {
 	defer wg.Done()
-	
+
 	log.Println(url)
 	doc, err := goquery.NewDocument(url)
 
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    name := doc.Find(".company_info .company_name a").First().Text()
-    site := doc.Find(".company_info .company_site a").Text()
-    about := doc.Find(".company_info .company_about").Text()
-    ratingstr := doc.Find(".company_info span.rating").Text()
-    address := doc.Find(".company_info .address").Text()
-	link, _  := doc.Find(".company_info .company_name a").Attr("href")
-
-	log.Println(name) 
-	var rating float32
-	value, err := strconv.ParseFloat(ratingstr, 32)
 	if err != nil {
-		log.Println(err)
-	} else {
-		rating = float32(value)
+		log.Fatal(err)
 	}
-    
+
+	name := doc.Find(".company_info .company_name a").First().Text()
+	site := doc.Find(".company_info .company_site a").Text()
+	about := doc.Find(".company_info .company_about").Text()
+	ratingstr := doc.Find(".company_info span.rating").Text()
+	address := doc.Find(".company_info .address").Text()
+	link, _ := doc.Find(".company_info .company_name a").Attr("href")
+
+	log.Println(name)
+	var rating float32
+	if len(ratingstr) > 0 {
+		value, err := strconv.ParseFloat(ratingstr, 32)
+		if err != nil {
+			log.Println(err)
+		} else {
+			rating = float32(value)
+		}
+	}
+
 	came := companiesList(doc, ".left_section .company_item")
 	left := companiesList(doc, ".right_section .company_item")
 
 	company := Company{
 		name,
 		site,
-	    about, 
+		about,
 		rating,
-		address, 
+		address,
 		0,
 		link,
 		left,
 		came,
 	}
-    c <- &company
+	c <- &company
 }
-
 
 func companiesList(doc *goquery.Document, path string) []Employee {
 	var employees []Employee
@@ -84,23 +84,23 @@ func companiesList(doc *goquery.Document, path string) []Employee {
 		page = BASE_URL + page
 		name := s.Find(".title").Text()
 		amountstr := s.Find(".count").Text()
-		amountstr = amountstr[: strings.Index(amountstr, " ")]
-        
+		amountstr = amountstr[:strings.Index(amountstr, " ")]
+
 		var amount int64
-	    amount, err := strconv.ParseInt(amountstr, 10, 32)
-	    if err != nil {
+		amount, err := strconv.ParseInt(amountstr, 10, 32)
+		if err != nil {
 			log.Println(err)
 		}
 
 		//log.Println(name, amount)
 		employees = append(
-			employees, 
+			employees,
 			Employee{
-				CompanyPage: page, 
-				CompanyName: name, 
-				Amount: int(amount),
-		})
-    })	
+				CompanyPage: page,
+				CompanyName: name,
+				Amount:      int(amount),
+			})
+	})
 
 	return employees
 }
