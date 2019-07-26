cd3401ab606b95f82413e8701ecce9b5bf8a6167
Rail Alimov <ra.alimov@gmail.com>
2019-07-26 22:30:05 +0700

    upd init.sql

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,11 +1,11 @@
 CREATE TABLE company (
-    name varchar(512) NOT NULL,  
-    site varchar(512),
+    name varchar(1024) NOT NULL,  
+    site varchar(1024),
     about varchar(1024),
     rating float,
-    address varchar(512),
+    address varchar(1024),
     score int,
-    link  varchar(512),
+    link  varchar(1024),
     employees_left json,
     employees_came json,
     id serial NOT NULL PRIMARY KEY
